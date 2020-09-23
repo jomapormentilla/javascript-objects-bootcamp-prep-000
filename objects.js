6b1092ca7ba79daee5d73129f0e170dd6bac6b34
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-23 19:08:22 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,6 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName
-  return playlist
+  if( delete playlist.artistName ){
+    return playlist
+  }
 }
